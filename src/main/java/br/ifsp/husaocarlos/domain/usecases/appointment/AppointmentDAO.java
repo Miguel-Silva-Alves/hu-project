c8f0a8987c57ceb3982cbb524be9a5f4f82c2eba
miguel <miguelsilvalves.2015@gmail.com>
2023-11-15 17:21:46 -0300

    :bug: fix: errors of merge fixed

--- src/main/java/br/ifsp/husaocarlos/domain/usecases/appointment/AppointmentDAO.java
+++ src/main/java/br/ifsp/husaocarlos/domain/usecases/appointment/AppointmentDAO.java
@@ -1,9 +1,6 @@
 package br.ifsp.husaocarlos.domain.usecases.appointment;
-
-<<<<<<< HEAD
-=======
 import br.ifsp.husaocarlos.domain.entities.Patient;
->>>>>>> 4c828c65fb4993b1cae5d34596c32ef8fb0fa95c
+
 import br.ifsp.husaocarlos.domain.entities.Student;
 import br.ifsp.husaocarlos.domain.entities.appointment.Appointment;
 import br.ifsp.husaocarlos.domain.usecases.utils.DAO;
@@ -12,8 +9,6 @@ import java.util.List;
 
 public interface AppointmentDAO extends DAO<Integer, Appointment> {
     List<Appointment> getAppointmentStudent(Student student);
-<<<<<<< HEAD
-=======
+
     List<Appointment> getAppointmentPatient(Patient patient);
->>>>>>> 4c828c65fb4993b1cae5d34596c32ef8fb0fa95c
 }

673f7263042bc69dd5ee3dfe3134235aea4c5f1d
Gabriel <gabriel.rufino@biotronica.tech>
2023-11-20 18:28:58 -0300

    feat: Export to csv use case and sunnyday test

---
+++ src/test/java/br/ifsp/husaocarlos/domain/usecases/management/ExportToCsvUseCaseTest.java
@@ -0,0 +1,31 @@
+package br.ifsp.husaocarlos.domain.usecases.management;
+import br.ifsp.husaocarlos.domain.entities.Professor;
+import br.ifsp.husaocarlos.domain.entities.Roles;
+import br.ifsp.husaocarlos.domain.entities.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.security.enterprise.credential.Password;
+
+
+public class ExportToCsvUseCaseTest {
+
+    private User currentUser;
+
+    @BeforeEach
+    public void setup(){
+        Password professorPassword = new Password("123456");
+        currentUser = new Professor("prof.educador@gmail.com","579.456.789-56","João",professorPassword,"la na pqp",null, Roles.Professor);
+    }
+
+    @Test
+    public void CreateAndExporteCSVFileWithSucess() {
+        ExportToCsvUseCase exportToCsvUseCase = new ExportToCsvUseCase(currentUser);
+        exportToCsvUseCase.setFileName("Test.csv");
+
+        exportToCsvUseCase.addRow(new String[]{"Nome", "Idade", "Cidade"});
+        exportToCsvUseCase.addRow(new String[]{"João", "25", "São Paulo"});
+        exportToCsvUseCase.addRow(new String[]{"Maria", "30", "Rio de Janeiro"});
+        exportToCsvUseCase.closeAndExportFile();
+    }
+}
